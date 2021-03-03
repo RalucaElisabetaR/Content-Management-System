5a6a5c52cf2d0d84ef29dbc5733685b3f454c4d8
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-03 18:35:35 +0100

    display data in admin category

--- admin/categories.php
+++ admin/categories.php
@@ -1,9 +1,9 @@
-<?php include "includes/header.php" ?>
+<?php include "includes/admin_header.php" ?>
 
 <div id="wrapper">
 
     <!-- Navigation -->
-    <?php include "includes/navigation.php" ?>
+    <?php include "includes/admin_navigation.php" ?>
 
     <div id="page-wrapper">
 
@@ -20,7 +20,28 @@
 
 
                     <div class="col-xs-6">
-                        <form action="">
+<?php
+if (isset($_POST['submit'])) {
+
+    $cat_title = $_POST['cat_title'];
+
+    if ($cat_title == "" || empty($cat_title)) {        
+        echo "This field should not be empty";
+    } else {
+        $query = "INSERT INTO categories(cat_title)";
+        $query .= "VALUE ('{$cat_title}') ";
+        $create_category_query = mysqli_query($connection, $query);
+        if (! $create_category_query) {
+           die('QUERY FAILED') . mysqli_error($connection);
+        }
+    }
+}
+
+
+?>
+
+
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat_title">Add Category</label>
                                 <input type="text" class="form-control" name="cat_title">
@@ -33,34 +54,45 @@
                     </div> <!-- Add Cathegory FORM -->
 
                     <div class="col-xs-6">
-                        <table class="table table-bordered table-hover
-                        ">
+                        <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
                                     <th>Id</th>
                                     <th>Category Title</th>
+                            
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Category 1</td>
-                                    <td>Category 2</td>
-                                </tr>
+ <?php
+$query = "SELECT * FROM categories ";
+$select_categories = mysqli_query($connection, $query);
+
+
+while ($row = mysqli_fetch_assoc($select_categories)) {
+    $cat_id= $row['cat_id'];
+    $cat_title = $row['cat_title'];
+    echo "<tr>";
+    echo "<td>{$cat_id}</td>";
+    echo "<td>{$cat_title}</td>";
+    echo "</tr>";
+}
+?>
+
                             </tbody>
                         </table>
 
                     </div>
 
-                    </div>
                 </div>
-                <!-- /.row -->
-
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
 
     </div>
-    <!-- /#wrapper -->
-    <?php include 'includes/footer.php' ?>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+<?php include 'includes/admin_footer.php' ?>
