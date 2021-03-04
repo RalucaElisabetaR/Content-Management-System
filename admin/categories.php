2d4d01bc698ab4800facf6d6d16806cabe4301bc
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-04 12:26:42 +0100

    created 'delete category' function and query

--- admin/categories.php
+++ admin/categories.php
@@ -63,7 +63,9 @@ if (isset($_POST['submit'])) {
                                 </tr>
                             </thead>
                             <tbody>
- <?php
+
+
+ <?php // FIND ALL CATEGORIES QUERY
 $query = "SELECT * FROM categories ";
 $select_categories = mysqli_query($connection, $query);
 
@@ -74,8 +76,20 @@ while ($row = mysqli_fetch_assoc($select_categories)) {
     echo "<tr>";
     echo "<td>{$cat_id}</td>";
     echo "<td>{$cat_title}</td>";
-    echo "</tr>";
+    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
 }
+?>
+
+
+<?php //DELETE QUERY 
+if (isset($_GET['delete'])) {   
+    $the_cat_id = $_GET['delete'];
+    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+    $delete_query = mysqli_query($connection, $query);
+    header("location: categories.php");
+}
+
+
 ?>
 
                             </tbody>
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,4 +1,6 @@
-<?php include "../includes/db.php" ?>
+<?php ob_start(); ?>
+
+<?php include "../includes/db.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
