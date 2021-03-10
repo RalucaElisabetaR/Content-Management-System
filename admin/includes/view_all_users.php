0d314daf6046b3892293cc2b0b74483c9728154a
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-10 20:44:41 +0100

    change user roles feature

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -66,8 +66,8 @@
 
 
 
-      echo "<td><a href='comments.php?approve='>Approve</a></td>";
-      echo "<td><a href='comments.php?reject='>Reject</a></td>";
+      echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
+      echo "<td><a href='users.php?change_to_guest_user={$user_id}'>Guest User</a></td>";
 
 
       echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
@@ -83,22 +83,22 @@
 <?php
 
 
-if (isset($_GET['approve'])) {
+if (isset($_GET['change_to_admin'])) {
 
-  $the_comment_id = $_GET['approve'];
-  $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id ";
-  $approve_comment_query = mysqli_query($connection, $query);
-  header("Location: comments.php");
+  $the_user_id = $_GET['change_to_admin'];
+  $query = "UPDATE users SET user_role = 'Admin' WHERE user_id = $the_user_id";
+  $change_to_admin_query = mysqli_query($connection, $query);
+  header("Location: users.php");
   exit;
 }
 
 
-if (isset($_GET['reject'])) {
+if (isset($_GET['change_to_guest_user'])) {
 
-  $the_comment_id = $_GET['reject'];
-  $query = "UPDATE comments SET comment_status = 'rejected' WHERE comment_id = $the_comment_id ";
-  $reject_comment_query = mysqli_query($connection, $query);
-  header("Location: comments.php");
+  $the_user_id = $_GET['change_to_guest_user'];
+  $query = "UPDATE users SET user_role = 'Guest User' WHERE user_id = $the_user_id ";
+  $change_to_guest_user_query = mysqli_query($connection, $query);
+  header("Location: users.php");
   exit;
 }
 
