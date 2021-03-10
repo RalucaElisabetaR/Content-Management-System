3ca3afc49cc68a59faa145a2fb2535dfa865b3d1
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-10 20:54:59 +0100

    setup the EDIT USER page

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -69,7 +69,7 @@
       echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
       echo "<td><a href='users.php?change_to_guest_user={$user_id}'>Guest User</a></td>";
 
-
+      echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
       echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
       echo "</tr>";
     }
