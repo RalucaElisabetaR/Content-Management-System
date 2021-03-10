558aa68a29afcf635fe1b6b03f5a3070a983f7b5
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-10 20:20:15 +0100

    delete users feature

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -70,7 +70,7 @@
       echo "<td><a href='comments.php?reject='>Reject</a></td>";
 
 
-      echo "<td><a href='comments.php?delete='>Delete</a></td>";
+      echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
       echo "</tr>";
     }
 
@@ -104,10 +104,10 @@ if (isset($_GET['reject'])) {
 
 
 if (isset($_GET['delete'])) {
-  $the_comment_id = $_GET['delete'];
-  $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id} ";
-  $delete_query = mysqli_query($connection, $query);
-  header("Location: comments.php");
+  $the_user_id = $_GET['delete'];
+  $query = "DELETE FROM users WHERE user_id = {$the_user_id} ";
+  $delete_user_query = mysqli_query($connection, $query);
+  header("Location: users.php");
   exit;
 }
 
