14c2d858bf55bb029f0310b2015d9dc8be519594
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-13 22:29:05 +0100

    add links to Post Image & Read More Button

--- index.php
+++ index.php
@@ -44,12 +44,15 @@
           <span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?>
         </p>
         <hr />
+
+        <a href="post.php?p_id=<?php echo $post_id; ?>"></a>
+
         <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="" />
         <hr />
         <p>
           <?php echo $post_content; ?>
         </p>
-        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
         <hr />
 
