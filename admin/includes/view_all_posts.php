3457302e857f338853049682b95e70df0dac2d14
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-14 00:17:16 +0100

    add BULK OPTION -published- functionality

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,14 +1,40 @@
+<?php
+if (isset($_POST['checkBoxArray'])) {
+  foreach ($_POST['checkBoxArray'] as $postValueId) {
+
+    $bulk_options = $_POST['bulk_options'];
+    switch ($bulk_options) {
+      case 'published':
+        $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = '$postValueId' ";
+        $updated_to_publishe_status = mysqli_query($connection, $query);
+        confirmQuery($updated_to_publishe_status);
+        break;
+
+      default:
+        # code...
+        break;
+    }
+  }
+}
+
+
+
+
+
+?>
+
+
 <form action="" method="post">
 
 
 
   <table class="table table-bordered table-hover">
     <div id="bulkOptionsContainer" class="col-xs-4">
-      <select class="form-control" name="" id="" class="">
+      <select class="form-control" name="bulk_options" id="" class="">
         <option value="">Select Option</option>
-        <option value="">Publish</option>
-        <option value="">Draft</option>
-        <option value="">Delete</option>
+        <option value="published">Publish</option>
+        <option value="draft">Draft</option>
+        <option value="delete">Delete</option>
       </select>
     </div>
     <div class="col-xs-4">
