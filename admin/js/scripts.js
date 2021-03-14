79544e8d387515c882085ddf56833be33d8fb712
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-14 14:53:39 +0100

    Bulk select All Posts JAVASCRIPT

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -8,5 +8,25 @@ $(document).ready(function() {
             console.error( error );
         } );
 
+  $('#selectAllBoxes').click(function(event){
+
+    if(this.checked) {
+      $('.checkBoxes').each(function(){
+      this.checked = true;
+
+     });
+
+  } else {
+    $('.checkBoxes').each(function(){
+      this.checked = false;
+    });
+
+  }
+
+
+  });
+
 
 });
+
+
