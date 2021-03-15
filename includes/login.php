c9d319f8323dfad93d763f058de4d45cee7cc1ca
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-15 10:19:21 +0100

    update CMS for password encryption

--- includes/login.php
+++ includes/login.php
@@ -22,23 +22,25 @@ if (isset($_POST['login'])) {
 
     $db_user_id = $row['user_id'];
     $db_username = $row['username'];
-    $db_password = $row['user_password'];
+    $db_user_password = $row['user_password'];
     $db_user_firstname = $row['user_firstname'];
     $db_user_lastname = $row['user_lastname'];
     $db_user_role = $row['user_role'];
   }
 
+  $password = crypt($password, $db_user_password);
 
-  if ($username === $db_username && $password === $db_password) {
 
+  if ($username !== $db_username && $password !== $db_user_password) {
+    header("Location: ../index.php");
+  } else if ($username == $db_username && $password == $db_user_password) {
     $_SESSION['username'] = $db_username;
     $_SESSION['firstname'] = $db_user_firstname;
-    $_SESSION['lastname'] = $db_luser_astname;
+    $_SESSION['lastname'] = $db_user_lastname;
     $_SESSION['user_role'] = $db_user_role;
 
+
     header("Location: ../admin ");
-  } else {
-    header("Location: ../index.php");
   }
 }
 
