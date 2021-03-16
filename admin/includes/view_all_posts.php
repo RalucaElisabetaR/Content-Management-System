d529fccaef734724c941c1ae17ba36b72cc5a243
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-16 16:56:14 +0100

    clone posts feature

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -100,7 +100,7 @@ if (isset($_POST['checkBoxArray'])) {
 
       <?php
 
-      $query = "SELECT * FROM posts";
+      $query = "SELECT * FROM posts ORDER BY gitpost_id DESC";
       $select_posts = mysqli_query($connection, $query);
       while ($row = mysqli_fetch_assoc($select_posts)) {
         $post_id = $row['post_id'];
