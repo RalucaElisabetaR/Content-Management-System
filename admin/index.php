9540c939c3b69dbf005f70ab318fd769dd65be77
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-17 16:41:06 +0100

    create PHP code and query

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,18 @@
 <div id="wrapper">
 
   <?php
+  $session = session_id();
+  $time = time();
+  $timeout_in_seconds = 60;
+  $timeout = $time - $timeout_in_seconds;
 
+  $query = "SELECT * FROM users_online WHERE session = '$session'";
+  $send_query = mysqli_query($connection, $query);
+  $count = mysqli_num_rows($send_query);
 
-
+  if ($count == NULL) {
+    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES ($session, $time)");
+  }
   ?>
 
 
