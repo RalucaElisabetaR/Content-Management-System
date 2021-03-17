903edef002f2c4ead3aa9a5b8a73225eb42d457a
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-17 17:57:46 +0100

    display users_online in admin

--- admin/index.php
+++ admin/index.php
@@ -13,8 +13,13 @@
   $count = mysqli_num_rows($send_query);
 
   if ($count == NULL) {
-    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES ($session, $time)");
+    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time' )");
+  } else {
+    mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session' ");
   }
+  $users_online_query = mysqli_query($connection, "SELECT * FROM  users_online WHERE time > '$timeout' ");
+  $count_users = mysqli_num_rows($users_online_query);
+
   ?>
 
 
@@ -33,6 +38,7 @@
             Welcome to Admin,
             <small> <?php echo $_SESSION['username']; ?> </small>
           </h1>
+          <?php echo $count_users; ?>
 
         </div>
       </div>
